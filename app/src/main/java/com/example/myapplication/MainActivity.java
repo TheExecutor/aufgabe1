bfe0b530f67ca9378b135431dd992864401f55e7
Alpen Adria Universität Klagenfurt <corinnagr@edu.aau.at>
2019-03-12 10:16:35 +0100

    declared Edittext and TextView for User enter and output. Programmed reading from user enter in OnClickListener

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,11 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnSendToServer = findViewById(R.id.btnSendToServer);
     Button btnSortMatNumber = findViewById(R.id.btnSortMatNumber);
+
+    EditText etxtMatNumber = findViewById(R.id.etxtMatNumber);
+
+    TextView txtvSortedMatNumber = findViewById(R.id.txtvSortedMatNumber);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +24,15 @@ public class MainActivity extends AppCompatActivity {
         btnSortMatNumber.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                String matNumber = "";
+                int[] sortedMatNumber = new int[8];
+                matNumber = etxtMatNumber.getText().toString();
+
+                for(int i = 0; i < sortedMatNumber.length; i++){
+                        sortedMatNumber[i] = Character.getNumericValue(matNumber.charAt(i));
+                }
+
+
             }
         });
     }
