a106857eefeba023f61df662bf319b206f523000
Alpen Adria Universität Klagenfurt <corinnagr@edu.aau.at>
2019-03-12 10:06:19 +0100

    declared Buttons and OnClickListener for Button SortMatNumber

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSendToServer = findViewById(R.id.btnSendToServer);
+    Button btnSortMatNumber = findViewById(R.id.btnSortMatNumber);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSortMatNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
